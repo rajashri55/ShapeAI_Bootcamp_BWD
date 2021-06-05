26e42700eb3c965833a366a804a69b7898b22fde
rajashri55 <85394694+rajashri55@users.noreply.github.com>
2021-06-05 18:02:57 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>ShapeAI</h1>
+      <img scr="https://avatars.githubusercontent.com/u/69104283?s=200&v=4" alt="ShapeAI logo" />
+
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1> Javascript and React.js</h1>
+      <img
+        scr="https://1.bp.blogspot.com/-YEGvmG8moqU/YHmeJAT6vDI/AAAAAAAABW8/iGOBao2eRvoU8mZJWwfswB3VZeV_eXBcQCLcBGAsYHQ/w640-h358/IMG_20210416_195135.jpg"
+        alt="7 Days Long Live Classes"
+      />
+
+      <p>
+        This was an amazing bootcamp taken up by Shaurya Sinha Sir. He covered
+        everything from Scratch including Javascript React.js, HTML. And his
+        explanation was awesome. He also cleared every doubts we had. ThankYou
+        so much sir for teaching us.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
